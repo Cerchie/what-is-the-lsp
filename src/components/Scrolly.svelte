ec2b87c6d6849eb4557ef41ad1810305efe63f77
Cerchie <luciacerchie@gmail.com>
2023-11-21 16:22:43 -0800

    fix server request spacing

--- src/components/Scrolly.svelte
+++ src/components/Scrolly.svelte
@@ -106,27 +106,25 @@ observer.observe(document.querySelector('.image.image-1'));
     <ToolTip/>
 </figure>
 </section>
-<section id="server-requests">
 
+<section id="server-requests">
+    <figure>
     <h3>Server Requests</h3>
-    <p>When you click on <span class="codespan">goToDefinition</span> you trigger a request from the editor, the client, to the server.
-        This request is for the<span class="codespans">textDocument/definition</span> 
-        and includes the parameters <span class="codespan">{'{documentURI, position}'}</span>.
+    <p>When you click on <span class="codespan">goToDefinition</span> you trigger a request from the editor, the client, to the server. This request is for the<span class="codespans">textDocument/definition</span> and includes the parameters <span class="codespan">{'{documentURI, position}'}</span>.
     </p>
     <p>The server then responds with the <span class="codespan">Location</span> of the definition, so the client can send you there.</p>
     <p>(And because it can handle these requests async, it can handle multiple requests at once.)</p>
     <GoToDef/>
 <p>In this way, the Language Server Protocol handles requests from code editors to support features for different languages in each client.</p>
-
+</figure>
 </section>
+
 <section id="resources">
-   
-    <h3 id="h3-resources">Resources</h3>
 <figure>
+    <h3>Resources</h3>
     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" id="blob">
         <path fill="#f58607" d="M29.6,-43.5C39.8,-39.4,50.6,-33.7,55.4,-24.8C60.3,-15.9,59.2,-3.8,55.8,6.8C52.3,17.4,46.6,26.4,39.3,33.3C32.1,40.1,23.3,44.9,13.5,49.1C3.7,53.4,-7.1,57.2,-17,55.4C-26.8,53.6,-35.8,46.1,-47.7,38.1C-59.7,30.1,-74.6,21.5,-74.9,11.5C-75.2,1.5,-60.7,-9.8,-50.3,-18.9C-39.9,-28.1,-33.4,-35,-25.7,-40.3C-18,-45.6,-9,-49.1,0.3,-49.7C9.7,-50.2,19.3,-47.7,29.6,-43.5Z" transform="translate(100 100)" />
       </svg>
-
     <p>This page has provided a high-level overview -- it's definitely the very tip of the iceberg, even conceptually.
         Here are some resources to help you dive deeper.
     </p>
@@ -265,9 +263,7 @@ h4 {
   width: 100vw;
 }
 
-#h3-resources {
-    margin-left: .9em;
-}
+
 
 .spacer {
 	height: 5vh;
@@ -281,12 +277,6 @@ h4 {
 @keyframes fadeOut {
 0% { opacity: 1; }
 100% { opacity: 0; }
-}
-
-#server-requests {
-    width: 80vw;
-	margin: auto;
-    margin-left: 5em;
 }
 
 #about-para {
@@ -381,9 +371,6 @@ section {
     font-family: 'Merriweather', serif;
     }
 
-    #h3-resources {
-    margin-left: 6em;
-}
 
 
 hr {
